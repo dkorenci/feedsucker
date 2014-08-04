2f4c9a757a91b6b71d6b2cdc088ea9a4d6541e9e
dam1root <dam1root@batou>
2014-08-04 16:38:37 +0200

    configuration constants

---
+++ src/rsssucker/config/RssConfig.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rsssucker.config;
+
+/**
+ *
+ */
+public class RssConfig {
+    public static String propertiesFile = "config/rsssucker.properties";
+}
