7000ddae972929298949899a9d5b963239588d51
dam1root <dam1root@batou>
2014-08-04 16:38:52 +0200

    JPA utility classes

---
+++ src/rsssucker/data/Factory.java
@@ -0,0 +1,15 @@
+package rsssucker.data;
+
+import javax.persistence.Persistence;
+
+
+public class Factory {
+
+    public static JpaContext createContext() {
+        JpaContext jpa = new JpaContext();
+        jpa.emf = Persistence.createEntityManagerFactory("rsssuckerPU");
+        jpa.em = jpa.emf.createEntityManager();
+        return jpa;
+    }
+    
+}
---
+++ src/rsssucker/data/JpaContext.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rsssucker.data;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ * Utility class, for accessing and closing EntityManagerFactory and EntityManager;
+ */
+public class JpaContext {
+
+    EntityManagerFactory emf;
+    public EntityManager em;
+    
+    public void close() {
+        em.close();
+        emf.close();
+    }
+    
+}
