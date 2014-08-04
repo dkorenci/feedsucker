ac9d084845f8c8eeb136fb20d50e0eb0009ac442
dam1root <dam1root@batou>
2014-08-04 16:39:33 +0200

    url made unique

--- src/rsssucker/data/NewsArticle.java
+++ src/rsssucker/data/NewsArticle.java
@@ -10,12 +10,17 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Temporal;
 
 /** Raw news article data downloaded from the web. */
 @Entity
+@NamedQueries({
+    @NamedQuery(name = "getArticlesAfterDate", 
+        query = "SELECT a FROM NewsArticle a WHERE a.datePublished >= :date")
+})
 public class NewsArticle {
-
     @Id
     @GeneratedValue
     private Long id;
@@ -32,8 +37,8 @@ public class NewsArticle {
     @Column(length=1000)
     private String title;
     
-    @Column(length=10000)
-    String url;
+    @Column(length=10000,unique = true)
+    private String url;
     
     public Date getDatePublished() {
         return datePublished;
@@ -73,6 +78,14 @@ public class NewsArticle {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
     
 }
