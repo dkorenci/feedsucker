2b51cf48638b4ebc91dc12472cf321470c5c993f
Damir Korencic <damir.korencic@gmail.com>
2014-08-07 19:05:10 +0200

    interactive shell outputing article data created

--- newspaper/extract.py
+++ newspaper/extract.py
@@ -1,5 +1,6 @@
 import newspaper
 from newspaper import Article
+import sys
 
 def processSite(siteURL):
     site = newspaper.build(siteURL)
@@ -26,13 +27,27 @@ def processArticle(articleURL):
     print "KEYWORDS:"    
     print article.keywords
 
+def printArticleText(articleURL):
+    try:
+        article = Article(url=articleURL)
+        article.download()
+        article.parse()
+        print article.title
+        print article.text
+    except: 
+        print "ERROR: "    
 
 def run():    
     #processArticle("http://edition.cnn.com/2014/07/27/world/meast/mideast-crisis-reporters-notebook/index.html?eref=edition")
     #processArticle("http://edition.cnn.com/video/data/2.0/video/world/2014/07/28/pkg-sidner-gaza-un-school-strike.cnn.html")
     #processArticle("http://www.theguardian.com/environment/2014/jul/28/bee-research-funding-pesticides-mps")
     #processArticle("http://www.indystar.com/story/news/local/hamilton-county/2014/07/27/carmel-couple-die-apparent-murder-suicide/13255503/")
-    processArticle("http://www.tristate-media.com/pdclarion/article_3793b120-12ed-11e4-9c26-001a4bcf887a.html")
+    #processArticle("http://www.tristate-media.com/pdclarion/article_3793b120-12ed-11e4-9c26-001a4bcf887a.html")
+    while True:        
+        line = sys.stdin.readline()
+        if line == "EXIT\n" : break
+        else : printArticleText(line)        
+
     
 run()    
         
