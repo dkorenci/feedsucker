080c460d442c0d7a5f60e9584dc466d47285a13f
damir <damir@batou>
2014-11-07 17:57:51 +0100

    new creation scripts, unfinished

---
+++ db_scripts/create_database.sh
@@ -0,0 +1,13 @@
+# create a database using create_database.sql script
+# database name and user name are taken from command line and substituted in the script
+# user executing script must be able to sudo as postgres (default admin account that must exist)
+DATABASE=$1
+USER=$2
+DB_COMMAND=`cat create_database_test.sql`
+DB_COMMAND=${DB_COMMAND/'$USER'/$USER}
+DB_COMMAND=${DB_COMMAND/'$DATABASE'/$DATABASE}
+echo $DB_COMMAND
+sudo -u postgres psql -c "$DB_COMMAND"
+#foo="Hello"
+#foo="$foo World"
+#echo $foo
