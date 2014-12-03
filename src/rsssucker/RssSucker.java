330270f3970eabfedd7bb403e30c8aac9cceab6e
damir <damir@batou>
2014-12-03 18:09:43 +0100

    apache httpclient library and test code added

--- src/rsssucker/RssSucker.java
+++ src/rsssucker/RssSucker.java
@@ -10,14 +10,26 @@ import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.XmlReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.apache.http.Header;
+import org.apache.http.HttpHost;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.client.utils.URIUtils;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.html.HtmlParser;
@@ -52,13 +64,45 @@ public class RssSucker {
     private static final Logger logger = LoggersManager.getErrorLogger(RssSucker.class.getName());
     
     public static void main(String[] args) throws Exception {        
-        //DataOperations.test();
-        //testNewspaper();
-        //testLogging();
-        testMediadef();
-        MediadefPersister.printOutletsAndFeeds();
-        //testNewspaper2();
+        testUrlRedirect();
     }
+
+    private static void testUrlRedirect() throws IOException, URISyntaxException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpClientContext context = HttpClientContext.create();
+        String uri = "http://news.google.com/news/url?sr=1&ct2=us%2F0_0_s_0_12_a&sa=t&usg=AFQjCNFcJYqcno20DdIVUEAwgeSb19w6_g&cid=52778672338616&url=http%3A%2F%2Fwww.msnbc.com%2Fmsnbc%2Fmike-browns-stepdad-apologizes-outburst&ei=hDB_VIC-FYjA1Aa1xoGABQ&rt=HOMEPAGE&vm=STANDARD&bvm=section&did=-2511018995557392677&ssid=h&gcnid=840";
+        //String uri="http://feeds.nbcnews.com/c/35002/f/663303/s/410be3e0/sc/1/l/0L0Snbcnews0N0Cpolitics0Cfirst0Eread0Csimple0Ecommon0Esense0Ejeh0Ejohnson0Edefends0Eexecutive0Eaction0Eimmigration0En259736/story01.htm";
+        //String uri="http://feeds.theguardian.com/c/34708/f/663879/s/410c0702/sc/8/l/0L0Stheguardian0N0Cworld0C20A140Cdec0C0A20Cnorth0Ekorea0Esony0Ecyber0Eattack/story01.htm";
+        //String uri = "http://feeds.reuters.com/~r/Reuters/worldNews/~3/ySVJ_LFYBrs/story01.htm";
+        //String uri = "http://rss.nytimes.com/c/34625/f/642565/s/40f27c2a/sc/20/l/0L0Snytimes0N0C20A140C110C290Cworld0Cmiddleeast0Cpalestinian0Ehaven0Efor0E60Edecades0Enow0Eflooded0Efrom0Esyria0E0Bhtml0Dpartner0Frss0Gemc0Frss/story01.htm";
+        HttpGet httpget = new HttpGet(uri);
+        httpget.addHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20121202 Firefox/17.0 Iceweasel/17.0.1");        
+        CloseableHttpResponse response = httpclient.execute(httpget, context);          
+        try {
+            HttpHost target = context.getTargetHost();
+            System.out.println("httpget URI: " + httpget.getURI());
+            System.out.println("target host: " + target);
+            System.out.println(response.getStatusLine().getStatusCode());            
+            for (Header h : response.getAllHeaders()) {
+                System.out.println(h.getName()+ " : " + h.getValue());                
+            }
+//            BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+//            String line = null;
+//            while ((line = br.readLine()) != null) {
+//                System.out.println(line);
+//            }
+            List<URI> redirectLocations = context.getRedirectLocations();
+            if (redirectLocations != null)
+            for (URI loc : redirectLocations) {
+                System.out.println("redirect location: " + loc);
+            }
+            URI location = URIUtils.resolve(httpget.getURI(), target, redirectLocations);
+            System.out.println("Final HTTP location: " + location.toASCIIString());
+            // Expected to be an absolute URI
+        } finally {
+            response.close();
+        }
+    }    
     
     public static void testNewspaper2() throws Exception {
         (new NewspaperTester()).concurrencyTest(10, 500);
