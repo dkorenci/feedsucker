affa96df63337e8cc2cee1db2e14be176684565f
damir <damir@batou>
2014-12-09 14:40:35 +0100

    custom logging interface

---
+++ src/rsssucker/log/RssSuckerLogger.java
@@ -0,0 +1,34 @@
+package rsssucker.log;
+
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Wrapper that contains all the necessary loggers and interface methods.
+ */
+public class RssSuckerLogger {
+    
+    private final Logger errLogger;            
+    private final Logger infoLogger;  
+    
+    public RssSuckerLogger(String className) {
+        errLogger = LoggersManager.getErrorLogger(className);
+        infoLogger = LoggersManager.getInfoLogger(className);                     
+    }
+    
+    public void logErr(String msg, Exception e) {
+        errLogger.log(Level.SEVERE, msg, e);        
+    }    
+    
+    public void logInfo(String msg, Exception e) {
+        infoLogger.log(Level.INFO, msg, e);        
+    }     
+    
+    // send info message
+    public void info(String msg) { 
+        String header = String.format("[%1$td%1$tm%1$tY_%1$tH:%1$tM:%1$tS] : ", new Date());
+        System.out.println(header + msg); 
+    }       
+    
+}
