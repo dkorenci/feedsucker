f2eb701f5b2422f2ee06c0ccb413c8f98c280167
damir <damir@damir.com>
2015-08-31 15:09:06 +0200

    ...

--- scripts/analyze_data.py
+++ scripts/analyze_data.py
@@ -156,7 +156,7 @@ def plotDates(dates):
     plt.show()
 
 ERRURL = '/data/rsssucker_data/errorUrlAll_topus05022015.txt'
-TABLE = '/datafast/rsssucker_data/table_topus_13042015.txt'
+TABLE = '/datafast/rsssucker_data/table_topus1_28082015.txt'
 #TABLE = '/data/rsssucker_data/table_topus03022015.txt'
 #TABLE='/data/rsssucker_data/table_loop_test_mediagg_23022015.txt'
 
