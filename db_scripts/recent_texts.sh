c615030e7c4074e241e4bbb6af8b5661a8e5b9b6
dkorenci <damir.korencic@gmail.com>
2015-08-31 15:00:39 +0200

    Script for listing save dates and beginnings of texts for top N most recently saved articles

---
+++ db_scripts/recent_texts.sh
@@ -0,0 +1,11 @@
+# arg1 database arg2 number of texts
+# print texts and titles ordered descending by datesaved
+DATABASE=$1
+NUMT=$2
+#DB_COMMAND="COPY \
+#(SELECT datesaved, substring(text from 1 for 100) FROM feedarticle ORDER BY datesaved DESC LIMIT $NUMT) \
+#TO STDOUT WITH CSV" 
+DB_COMMAND="SELECT datesaved, substring(text from 1 for 100) FROM feedarticle ORDER BY datesaved DESC LIMIT $NUMT;"
+echo $DB_COMMAND
+echo $DB_COMMAND | psql --username rsssucker --dbname $1
+#sudo -u postgres psql -c @$DB_COMMAND"
